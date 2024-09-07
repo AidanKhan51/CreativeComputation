3b10a4f03309d2714d0978f50b6230bc4840c9f7
Aidan Khan <aidan.khan31@gmail.com>
2024-09-07 13:33:30 -0400

    Finished FunctionCalls

--- Topics/Functioncalls/js/script.js
+++ Topics/Functioncalls/js/script.js
@@ -2,7 +2,7 @@
  * FunctionCalls
  * Aidan Khan
  * 
- * Function Calls practice
+ * Function Calls practice examples
  * please add a grade if this work is graded
  */
 
@@ -12,7 +12,9 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    //happens once at beginning of project
+    createCanvas(640, 480);
+    //function information (eg. 640, 480) is called an "arguement"
 }
 
 
@@ -20,5 +22,7 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-
+    //happens every frame program runs; like a loop
+    background(255, 100, 100);
+    rect(200, 80, 240, 320);
 }
