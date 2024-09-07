46bcd41bd559299a1e6d0ca56e98a52a48a5eac6
Aidan Khan <aidan.khan31@gmail.com>
2024-09-07 16:44:41 -0400

    started drawing exercise

---
+++ Topics/template-p5-project/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Drawing 
+ * Aidan Khan
+ * 
+ * Drawing practice
+ * please add a grade 
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
