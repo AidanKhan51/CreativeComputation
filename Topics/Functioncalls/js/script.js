c7de7aad5f34a698a3cc621a79145d0965014ef4
Aidan Khan <aidan.khan31@gmail.com>
2024-09-07 13:42:47 -0400

    Commenting practice finished

--- Topics/Functioncalls/js/script.js
+++ Topics/Functioncalls/js/script.js
@@ -2,27 +2,34 @@
  * FunctionCalls
  * Aidan Khan
  * 
- * Function Calls practice examples
+ * exploration of existential dread of a novelist, confronting a new world they have to create
+ * 
+ * this program is non interactive to convey the inability to get started on a project. Try typing or something.
+ * 
  * please add a grade if this work is graded
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates canvas for salmon pink (evil) background that represents the horror of creation
 */
 function setup() {
     //happens once at beginning of project
+    //Create canvas at standard resolution
     createCanvas(640, 480);
     //function information (eg. 640, 480) is called an "arguement"
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws the writer's evil piece of paper that they can never ever write on
 */
 function draw() {
-    //happens every frame program runs; like a loop
+    //happens every frame program runs like a loop
+    //pink desktop
     background(255, 100, 100);
+    //evil piece of unwriteable paper
     rect(200, 80, 240, 320);
+
 }
