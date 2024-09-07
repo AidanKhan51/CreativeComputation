cc62c87fb0b9123eb416554f7c8a9b415cd69f72
Aidan Khan <aidan.khan31@gmail.com>
2024-09-07 18:01:35 -0400

    The House of House finished

--- Topics/starter-house/js/script.js
+++ Topics/starter-house/js/script.js
@@ -1,10 +1,10 @@
 /**
- * My House
- * Pippin Barr
+ * The House of House
+ * Pippin Barr & Aidan Khan
  * 
- * Draws a house with shapes.
+ * Draws a house (for the doctor) with shapes.
  * 
- * Disclaimer: Not actually my house.
+ * Disclaimer: Not actually my house. It is the doctor's.
  * 
  * Uses:
  * p5.js
@@ -14,16 +14,55 @@
 /**
  * Creates the canvas
  */
+
+
+
+function preload() {
+
+    img = loadImage('/assets/images/House.png');
+
+}
+
 function setup() {
     createCanvas(640, 480);
 }
+
+
+
 
 /**
  * Draws a house
  */
 function draw() {
+
+
     // The sky
     background(150, 200, 250);
+
+    drawCloud();
+
+    drawGround();
+
+    drawHouse();
+
+    drawRuff();
+
+    drawDrHouse();
+
+    drawWindow();
+
+    drawDoor();
+
+
+
+}
+
+/**
+ * Draws a cloud to block the sun
+ * 
+ */
+
+function drawCloud() {
 
     // A cloud
     push();
@@ -39,6 +78,14 @@ function draw() {
     ellipse(220, 120, 60, 60);
     pop();
 
+}
+
+/**
+ * Draws a ground for doc House to stand upon
+ */
+
+function drawGround() {
+
     // The ground
     push();
     noStroke();
@@ -46,12 +93,28 @@ function draw() {
     rect(0, 400, 640, 480);
     pop();
 
+}
+
+/**
+ * Draws the main body of the house for the doctor
+ */
+
+function drawHouse() {
+
     // The main body of the house
     push();
     noStroke();
     fill(250, 250, 200);
     rect(200, 240, 280, 180);
     pop();
+
+}
+
+/** 
+ * If you're looking for Mine, he's on the ruff
+ */
+
+function drawRuff() {
 
     // The roof
     push();
@@ -61,15 +124,42 @@ function draw() {
     triangle(180, 240, 340, 120, 500, 240);
     pop();
 
+}
+
+/**
+ * Draws a window for house to look out of
+ */
+
+function drawWindow() {
+
     // A window
     push();
     // You can also write colour names from the CSS standard in quotes
     // https://www.w3.org/wiki/CSS/Properties/color/keywords
     stroke("deeppink");
     strokeWeight(5);
-    fill("blanchedalmond");
+    noFill();
     rect(220, 260, 80, 80);
     pop();
+}
+
+/**
+ * MORE MOUSE BITES!!!
+ */
+
+function drawDrHouse() {
+
+    //The doctor
+    push();
+    image(img, 220, 260, 80, 80);
+    pop();
+
+
+}
+
+/**Draws a door for doc house to leave and enter through */
+
+function drawDoor() {
 
     // An entrace
 
@@ -86,4 +176,5 @@ function draw() {
     fill(255, 215, 0);
     ellipse(340, 360, 10, 10);
     pop();
+
 }
