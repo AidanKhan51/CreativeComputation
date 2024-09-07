95e67738119d0712c6fec9af817e08acaccd24f2
Aidan Khan <aidan.khan31@gmail.com>
2024-09-07 17:11:55 -0400

    Finished Evil Vinyl Drawing Project

--- Topics/template-p5-project/js/script.js
+++ Topics/template-p5-project/js/script.js
@@ -1,24 +1,47 @@
 /**
- * Drawing 
+ * Evil Vinyl that kills you
  * Aidan Khan
  * 
- * Drawing practice
+ * Evil Vinyl that will kill you, please beware
  * please add a grade 
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates canvas for the evil vinyl that kills you
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Desplays evil vinyl that can kill you
 */
 function draw() {
 
+    //grey background for killer evil vinyl
+    background(150);
+
+    //red body for evil vinyl and white outline that can kill you btw
+    push();
+    fill(255, 0, 0);
+    stroke(255);
+    ellipse(320, 320, 480);
+    pop();
+
+    //white label at center of the evil killer vinyl
+    push();
+    fill("white");
+    noStroke();
+    ellipse(320, 320, 140)
+    pop();
+
+    //center killer donut hole for the dubious vinyl
+    push();
+    fill("#000000")
+    noStroke();
+    ellipse(320, 320, 20);
+    pop();
 }
