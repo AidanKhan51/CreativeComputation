d63624c477c8b516530b17c63f06576f4e5dd36c
Aidan Khan <158544280+AidanKhan51@users.noreply.github.com>
2024-09-10 15:12:26 -0400

    Creating-Variables module

---
+++ Topics/Creating Variables/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Creating variables
+ * Aidan Khan
+ * 
+ * Creating variables practice
+ * add a grade please
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
