5dceff783d6cabda9f0ab92e39d10e7332943d77
Aidan Khan <aidan.khan31@gmail.com>
2024-09-12 14:45:41 -0400

    Finished UFO project

--- Topics/ufo-on-a-dark-night/js/script.js
+++ Topics/ufo-on-a-dark-night/js/script.js
@@ -1,6 +1,6 @@
 /**
  * UFO on a Dark Night
- * Pippin Barr
+ * Pippin Barr & Aidan Khan
  * 
  * A UFO. On a dark night. It just sits there?
  */
@@ -33,8 +33,22 @@ function setup() {
  * Displays a UFO
 */
 function draw() {
+    //dawn
+    skyShade = skyShade + 1;
     // Display the sky
     background(skyShade);
+
+    //Move UFO
+    ufo.x = ufo.x + 0.5;
+    ufo.y = ufo.y - 2;
+
+    //change fill
+    ufo.fill = ufo.fill * 0.995;
+
+    //change dimensions
+    ufo.width = ufo.width / 1.005;
+
+    ufo.height = ufo.height / 1.005;
 
     // Draw the UFO based on its properties
     push();
