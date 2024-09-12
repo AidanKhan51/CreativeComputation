2f911e02207e4dcf7606e0fba4d16f71f346c65e
Aidan Khan <aidan.khan31@gmail.com>
2024-09-12 15:05:11 -0400

    started Variables in practice - movement

---
+++ Topics/template-p5-project - Copy/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Variables in practice
+ * Aidan Khan 
+ * 
+ * please add grade
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
