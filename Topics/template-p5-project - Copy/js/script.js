acc0e841fa40b3199ec5614457b592a6fa86ce79
Aidan Khan <aidan.khan31@gmail.com>
2024-09-12 15:33:29 -0400

    Bird variables practice finished

--- Topics/template-p5-project - Copy/js/script.js
+++ Topics/template-p5-project - Copy/js/script.js
@@ -7,10 +7,41 @@
 
 "use strict";
 
+let bird = {
+
+    x: 120,
+    y: 480,
+    size: 50,
+    velocity: {
+        x: 0,
+        y: 0
+    },
+
+    minVelocity: {
+
+        x: -3,
+        y: -2
+    },
+
+    maxVelocity: {
+
+        x: 3,
+        y: 2
+    },
+
+    acceleration: {
+        x: 0.025,
+        y: -0.05
+
+    }
+};
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
+    createCanvas(640, 480);
+
 
 }
 
@@ -19,5 +50,25 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+
+    background(0);
+
+    //move the bird
+
+    bird.velocity.x += + bird.acceleration.x;
+    bird.velocity.y += bird.acceleration.y;
+
+    bird.velocity.x = constrain(bird.velocity.x, bird.minVelocity.x, bird.maxVelocity.x);
+    bird.velocity.y = constrain(bird.velocity.y, bird.minVelocity.y, bird.maxVelocity.y);
+
+    bird.x += bird.velocity.x;
+    bird.y += bird.velocity.y;
+
+    //draw the bird
+    push();
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(bird.x, bird.y, bird.size)
+    pop();
 
 }
