4040899ffa1ee5b76c23b11899b7fb14f7057d12
Aidan Khan <aidan.khan31@gmail.com>
2024-09-12 15:00:42 -0400

    Constrained ego

--- Topics/constrain-that-ego/js/script.js
+++ Topics/constrain-that-ego/js/script.js
@@ -1,6 +1,6 @@
 /**
  * Constrain that Ego!
- * Pippin Barr
+ * Pippin Barr & Aidan Khan
  * 
  * This ego is out of control...
  */
@@ -12,7 +12,9 @@ let ego = {
     x: 200,
     y: 200,
     size: 1,
-    fill: 200
+    fill: 200,
+    minSize: 0,
+    maxSize: 400
 };
 
 /**
@@ -34,6 +36,7 @@ function draw() {
     ego.fill = ego.fill - 0.5;
     // Size gets bigger, symbolizing taking up emotional space
     ego.size = ego.size + 1;
+    ego.size = constrain(ego.size, ego.minSize, ego.maxSize);
 
     // Draw the ego
     push();
