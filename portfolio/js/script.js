7b2ca7bd538e59ec76cbb07bd7aa8c4533eaa73e
Aidan Khan <aidan.khan31@gmail.com>
2024-10-16 15:37:36 -0400

    Added Portfolio Project
    -Created Grid for different sections
    -Created fake popups for website

---
+++ portfolio/js/script.js
@@ -0,0 +1,69 @@
+/**
+ * Title of Project
+ * Author Name
+ */
+
+"use strict";
+
+const opened = new Set();
+
+function makePopup(contentId) {
+	if (opened.has(contentId)) return;
+	opened.add(contentId);
+
+	const popup = document.createElement('div');
+	popup.className = 'popup';
+
+	const topBar = document.createElement('div');
+	topBar.className = 'top-bar';
+	popup.prepend(topBar);
+
+	const contentCnt = document.createElement('div');
+	contentCnt.className = 'content-cnt';
+	contentCnt.innerHTML = document.getElementById(contentId).innerHTML;
+
+	popup.append(topBar, contentCnt);
+	document.body.append(popup);
+
+	popup.style.top = (window.innerHeight - popup.clientHeight) / 2 + 'px';
+	popup.style.left = (window.innerWidth - popup.clientWidth) / 2 + 'px';
+
+	let isMousedown = false;
+
+	let mouseUpListener;
+
+	function mouseMoveListener(e) {
+		if (isMousedown) {
+			popup.style.top = parseInt(popup.style.top) + e.movementY + 'px';
+			popup.style.left = parseInt(popup.style.left) + e.movementX + 'px';
+		}
+	}
+
+	topBar.addEventListener('mousedown', () => {
+		isMousedown = true;
+		mouseUpListener = () => isMousedown = false;
+		window.addEventListener('mouseup', mouseUpListener);
+	});
+
+	const closeBtn = document.createElement('button');
+	closeBtn.innerHTML = '&times';
+	topBar.append(closeBtn);
+
+	// Change to add listener to button
+	closeBtn.addEventListener('click', () => {
+		opened.delete(contentId);
+		popup.remove();
+		window.removeEventListener('mousemove', mouseMoveListener);
+		if (mouseUpListener) {
+			window.removeEventListener('mouseup', mouseUpListener);
+		}
+	});
+
+	window.addEventListener('mousemove', mouseMoveListener)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	document.getElementById('3d-btn').addEventListener('click', () => {
+		makePopup('3d-popup');
+	});
+});
