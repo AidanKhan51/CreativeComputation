c427e269c13a836e81fa8f6119069c54df6d7731
Aidan Khan <aidan.khan31@gmail.com>
2024-11-19 22:03:57 -0500

    Popup feature additions
    Popup features have been updated:
    -Popups can now be resized
    -Popups no longer travel off screen when dragged too far
    -Popups can be closed by pressing the original button
    -Popups can no longer be moved when resized too far
    -Popups now have a maximum and minimum size
    
    Additionally, some extra games have been added been, as well as some general HTML formatting.

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -5,69 +5,165 @@
 
 "use strict";
 
-const opened = new Set();
+const opened = new Map();
 
-const initializers = {'games-popup': () =>{
-	document.getElementById('hg3r-btn').addEventListener('click', () => {
-		makePopup('hg3r-popup');
-	});
-} }
+const initializers = {
+	'games-popup': () => {
+		document.getElementById('hg3r-btn').addEventListener('click', () => {
+			makePopup('hg3r-popup');
+
+		});
+		document.getElementById('TOOSLOW-btn').addEventListener('click', () => {
+			makePopup('TOOSLOW-popup');
+
+		});
+		document.getElementById('GAMBLE-btn').addEventListener('click', () => {
+			makePopup('GAMBLE-popup');
+
+		});
+	}
+}
 
 function makePopup(contentId) {
-	if (opened.has(contentId)) return;
-	opened.add(contentId);
+	if (opened.has(contentId)) {
+		opened.get(contentId).remove();
+		opened.delete(contentId);
+		return;
+	}
 
 	const popup = document.createElement('div');
 	popup.className = 'popup';
+	popup.innerHTML = document.getElementById('popup-base').innerHTML;
 
-	const topBar = document.createElement('div');
-	topBar.className = 'top-bar';
-	popup.prepend(topBar);
+	popup.querySelector('.content-cnt').innerHTML = document.getElementById(contentId).innerHTML;
 
-	const contentCnt = document.createElement('div');
-	contentCnt.className = 'content-cnt';
-	contentCnt.innerHTML = document.getElementById(contentId).innerHTML;
-
-	popup.append(topBar, contentCnt);
 	document.body.append(popup);
-
+	opened.set(contentId, popup);
 	initializers[contentId]?.()
 
 	popup.style.top = (window.innerHeight - popup.clientHeight) / 2 + 'px';
 	popup.style.left = (window.innerWidth - popup.clientWidth) / 2 + 'px';
 
-	let isMousedown = false;
-
-	let mouseUpListener;
-
-	function mouseMoveListener(e) {
-		if (isMousedown) {
-			popup.style.top = parseInt(popup.style.top) + e.movementY + 'px';
-			popup.style.left = parseInt(popup.style.left) + e.movementX + 'px';
+	registerResizer(popup, '.top-bar', e => {
+		const offsets = popup.getBoundingClientRect();
+		const barOffsets = popup.querySelector('.top-bar').getBoundingClientRect();
+		if (e.movementX > 0 || offsets.left > 0) {
+			popup.style.left = popup.style.left = (offsets.left + e.movementX) + 'px';
 		}
-	}
-
-	topBar.addEventListener('mousedown', () => {
-		isMousedown = true;
-		mouseUpListener = () => isMousedown = false;
-		window.addEventListener('mouseup', mouseUpListener);
+		if ((e.movementY > 0 || offsets.top > 0) && barOffsets.y + barOffsets.height + e.movementY <= window.innerHeight) {
+			popup.style.top = (offsets.top + e.movementY) + 'px';
+		}
 	});
-
-	const closeBtn = document.createElement('button');
-	closeBtn.innerHTML = '&times';
-	topBar.append(closeBtn);
 
 	// Change to add listener to button
-	closeBtn.addEventListener('click', () => {
+	popup.querySelector('.closeButton').addEventListener('click', () => {
 		opened.delete(contentId);
 		popup.remove();
-		window.removeEventListener('mousemove', mouseMoveListener);
-		if (mouseUpListener) {
-			window.removeEventListener('mouseup', mouseUpListener);
-		}
 	});
 
-	window.addEventListener('mousemove', mouseMoveListener)
+	registerResizer(popup, '.nw-corner', e => {
+		const movementY = e.movementY;
+		const movementX = e.movementX;
+		if (popup.clientHeight - e.movementY < 350) {
+			movementY = popup.clientHeight - 350;
+		}
+		if (popup.clientWidth - e.movementX < 350) {
+			movementX = popup.clientWidth - 350;
+		}
+		const offsets = popup.getBoundingClientRect();
+
+		popup.style.left = (offsets.left + movementX) + 'px';
+		popup.style.width = (popup.clientWidth - movementX) + 'px';
+
+		popup.style.top = (offsets.top + movementY) + 'px';
+		popup.style.height = (popup.clientHeight - movementY) + 'px';
+	});
+
+	registerResizer(popup, '.north-border', e => {
+		const movementY = e.movementY;
+		if (popup.clientHeight - e.movementY < 350) {
+			movementY = popup.clientHeight - 350;
+		}
+		const offsets = popup.getBoundingClientRect();
+
+		popup.style.top = (offsets.top + movementY) + 'px';
+		popup.style.height = (popup.clientHeight - movementY) + 'px';
+	});
+
+	registerResizer(popup, '.ne-corner', e => {
+		const movementY = e.movementY;
+		const movementX = e.movementX;
+		if (popup.clientHeight - e.movementY < 350) {
+			movementY = popup.clientHeight - 350;
+		}
+		if (popup.clientWidth + e.movementX < 350) {
+			movementX = popup.clientWidth + 350;
+		}
+		const offsets = popup.getBoundingClientRect();
+
+		popup.style.width = (popup.clientWidth + movementX) + 'px';
+
+		popup.style.top = (offsets.top + movementY) + 'px';
+		popup.style.height = (popup.clientHeight - movementY) + 'px';
+	});
+
+	registerResizer(popup, '.west-border', e => {
+		const movementX = e.movementX;
+		if (popup.clientWidth - e.movementX < 350) {
+			movementX = popup.clientWidth - 350;
+		}
+		const offsets = popup.getBoundingClientRect();
+
+		popup.style.left = (offsets.left + movementX) + 'px';
+		popup.style.width = (popup.clientWidth - movementX) + 'px';
+	});
+
+	registerResizer(popup, '.east-border', e => {
+		const movementX = e.movementX;
+		if (popup.clientWidth + e.movementX < 350) {
+			movementX = popup.clientWidth + 350;
+		}
+		popup.style.width = (popup.clientWidth + e.movementX) + 'px';
+	});
+
+	registerResizer(popup, '.sw-corner', e => {
+		const movementY = e.movementY;
+		const movementX = e.movementX;
+		if (popup.clientHeight + e.movementY < 350) {
+			movementY = popup.clientHeight + 350;
+		}
+		if (popup.clientWidth - e.movementX < 350) {
+			movementX = popup.clientWidth - 350;
+		}
+		const offsets = popup.getBoundingClientRect();
+
+		popup.style.left = (offsets.left + movementX) + 'px';
+		popup.style.width = (popup.clientWidth - movementX) + 'px';
+
+		popup.style.height = (popup.clientHeight + movementY) + 'px';
+	});
+
+	registerResizer(popup, '.south-border', e => {
+		const movementY = e.movementY;
+		if (popup.clientHeight + e.movementY < 350) {
+			movementY = popup.clientHeight + 350;
+		}
+		popup.style.height = (popup.clientHeight + movementY) + 'px';
+	});
+
+	registerResizer(popup, '.se-corner', e => {
+		const movementY = e.movementY;
+		const movementX = e.movementX;
+		if (popup.clientHeight + e.movementY < 350) {
+			movementY = popup.clientHeight + 350;
+		}
+		if (popup.clientWidth + e.movementX < 350) {
+			movementX = popup.clientWidth + 350;
+		}
+		popup.style.width = (popup.clientWidth + movementX) + 'px';
+
+		popup.style.height = (popup.clientHeight + movementY) + 'px';
+	});
 }
 
 // Popup commands from homepage
@@ -93,10 +189,24 @@ document.addEventListener('DOMContentLoaded', () => {
 		makePopup('games-popup');
 	});
 
-	document.getElementById('settings-btn').addEventListener('click', () => {
-		makePopup('settings-popup');
+	document.getElementById('bestOf-btn').addEventListener('click', () => {
+		makePopup('bestOf-popup');
 	});
-
 
 });
 
+function registerResizer(parent, selector, func) {
+	const el = parent.querySelector(selector);
+
+	el.addEventListener('mousedown', e => {
+		const mouseUp = () => {
+			window.removeEventListener('mousemove', func);
+			window.removeEventListener('mouseup', mouseUp);
+			document.body.style.userSelect = '';
+		};
+
+		window.addEventListener('mousemove', func);
+		window.addEventListener('mouseup', mouseUp);
+		document.body.style.userSelect = 'none';
+	});
+}
