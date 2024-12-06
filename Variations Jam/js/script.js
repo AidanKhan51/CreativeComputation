502d33123da177c77466a4400a64b654ddfe4f15
Aidan Khan <aidan.khan31@gmail.com>
2024-12-06 10:53:07 -0500

    Variations Jam - Initial commit

---
+++ Variations Jam/js/script.js
@@ -0,0 +1,30 @@
+/**
+ * Dartmaster
+ * Aidan Khan
+ * 
+ * Welcome to the Dart dome
+ * 
+ */
+
+"use strict";
+let dartsData;
+
+function preload() {
+    // Used in preload, loadJSON will just return the data into our variable
+    dartsData = loadJSON("assets/data/darts.json");
+}
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
