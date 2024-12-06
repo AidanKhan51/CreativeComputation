3662af0ace81846d250da7151b616b0471ac749a
Aidan Khan <aidan.khan31@gmail.com>
2024-12-06 12:58:17 -0500

    Created Dart turn system
    Created arrays that push "dart" objects into them at the position of mouseX and mouseY (when mouse is clicked). Then added two teams, red and blue, with darts pushed into their own separate arrays. Afterwards, created a system where the darts being fired (turns) would alternate after a team has fired 3 darts.

--- Variations Jam/js/script.js
+++ Variations Jam/js/script.js
@@ -8,23 +8,117 @@
 
 "use strict";
 let dartsData;
+let playerTurn;
+let calculateX;
+let calculateY;
+let textTurn;
+const BLUE = 1;
+const RED = 2;
+
+
+let dartsOne = [
+]
+
+let dartsTwo = [
+
+]
+
+class Dart {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 function preload() {
-    // Used in preload, loadJSON will just return the data into our variable
+    // turn data into variable
     dartsData = loadJSON("assets/data/darts.json");
 }
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * My Setup
 */
 function setup() {
-
+    createCanvas(800, 800);
+    playerTurn = BLUE;
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * My Draw
 */
 function draw() {
+    background("black");
+    checkTurn();
+    roundDisplay();
+    drawDarts();
+}
 
+function checkTurn() {
+    if (dartsTwo.length === (dartsOne.length)) {
+        playerTurn = BLUE;
+    }
+    else if (dartsOne.length === (dartsTwo.length + 3)) {
+        playerTurn = RED;
+    }
+}
+
+function mouseClicked() {
+    switch (playerTurn) {
+        case BLUE:
+            addDartBlue();
+            console.log(dartsOne[0], dartsOne[1], dartsOne[2])
+            console.log(dartsOne.length)
+            break;
+        case RED:
+            addDartRed();
+            console.log(dartsTwo[0], dartsTwo[1], dartsTwo[2])
+            console.log(dartsTwo.length)
+            break;
+    }
+}
+
+function roundDisplay() {
+    if (playerTurn === BLUE) {
+        push();
+        fill('blue');
+        textSize(20);
+        text('BLUE Turn', 50, 50);
+        pop();
+    }
+    else if (playerTurn === RED) {
+        push();
+        fill('red');
+        textSize(20);
+        text(' RED Turn', 50, 50);
+        pop();
+    }
+}
+
+function addDartBlue() {
+    calculateX = mouseX
+    calculateY = mouseY
+    dartsOne.push(new Dart(calculateX, calculateY));
+}
+
+function addDartRed() {
+    calculateX = mouseX
+    calculateY = mouseY
+    dartsTwo.push(new Dart(calculateX, calculateY));
+}
+
+function drawDarts() {
+    dartsOne.forEach(dart => {
+        push();
+        noStroke();
+        fill("blue");
+        ellipse(dart.x, dart.y, 50)
+        pop();
+    });
+    dartsTwo.forEach(dart => {
+        push();
+        noStroke();
+        fill("red");
+        ellipse(dart.x, dart.y, 50)
+        pop();
+    });
 }
